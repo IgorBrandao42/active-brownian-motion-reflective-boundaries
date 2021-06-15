844e21a47d77b02651b5654487edf2fd2a17c909
Igor Brandão <54045180+IgorBrandao42@users.noreply.github.com>
2021-06-14 22:22:45 -0300

    Save timestamps

--- ui/save_canvas.m
+++ ui/save_canvas.m
@@ -1,6 +1,6 @@
 function save_canvas()
 
-global user_obstacle_all user_particle_all h_obstacles h_particles_initial h_trajectories eta T
+global user_obstacle_all user_particle_all h_obstacles h_particles_initial h_trajectories eta T timestamps
 
 save("canvas_" + datestr(now,'dd-mmmm-yyyy_HH-MM-SS'), "user_obstacle_all", "user_particle_all", "h_obstacles", "h_trajectories", "h_particles_initial", "eta", "T", "timestamps");
 
